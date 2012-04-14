4383279ba751c8902e72f2ce763753451909dad4
Vitali <madgamer@Hot.ee>
2012-04-14 18:45:03 +0300

    edited column selection

--- app/components/question_grid/javascripts/init_component.js
+++ app/components/question_grid/javascripts/init_component.js
@@ -3,13 +3,18 @@
           this.superclass.initComponent.call(this);
 
           this.on('afterrender', function(self, eOpts) {
-              this.getSelectionModel().select(0);
+              answer_grid = Netzke.page.surveyApp.getChildNetzkeComponent('answer_grid')
+	      this.getStore().load();
           }, this);
 
+	 this.getStore().on('load', function(self, records, successful, operation, eOpts) {
+		 if ((records) && (records.length > 0)) {
+			this.getSelectionModel().select(0);
+		}
+	}, this);
+
              this.on('select', function(self, record, index, eOpts) {
-                Netzke.page.surveyApp.getChildNetzkeComponent('answer_grid').fireEvent('selectform', record.data.id, record.data.text);
+                answer_grid.fireEvent('selectform', record.data.id, record.data.text);
           }, this);
       }
 }
-
-
